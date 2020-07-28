25ed11fd3a5e560b10f77e91014722f47dcd9ecd
Orkun Berk Yüzbaşıoğlu <orkunberk@hotmail.com>
2020-07-28 23:27:24 +0300

    Add files via upload

---
+++ MyStack.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+class MyStack:
+    """ Stack implementation using lists """
+
+    def __init__(self):
+        """ create an empty stack """
+        self.data = []
+
+    def get_size(self):
+        return(len(self.data))
+
+    def is_empty(self):
+        return(len(self.data)==0)
+
+    def push(self, e):
+        self.data.append(e)
+
+    def top(self):
+        if(len(self.data)>0):
+            return(self.data[-1])
+
+    def pop(self):
+        if(len(self.data)>0):
+            return(self.data.pop())
+        else:
+            return('<<<stack is empty>. cannot pop an element>>')
+
+    def print_stack(self):
+        if(len(self.data)>0):
+            return(''.join(str(x) for x in self.data))
+        else:
+            return('<<< empty stack >>>')
+
---
+++ balanced_paranthesis.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+from MyStack import MyStack
+
+FILENAME = 'input.txt'
+pattern = ['(', '[' , '{']
+antipattern = [')', ']', '}']
+negative = {')':'(', ']':'[' , '}':'{'}
+
+with open(FILENAME) as f:
+    content = f.readlines()
+ 
+content = [x.strip() for x in content] 
+   
+for line in content:
+    
+    stack = MyStack()
+    
+    print('------------------------------------------------------------------------')
+    print('Input  : ', line)
+    
+    isCorrect = True
+    isString = False
+    
+    for c in  line:
+            
+        if(c == "'"):
+            isString = not isString
+            
+        if(isString):
+            continue
+        
+        else:    
+            if(c in pattern):
+                stack.push(c)
+                print('Open     : Push ', c, 'Stack Contents: ', stack.print_stack())
+                
+            elif(c in antipattern):
+                if(stack.top() == negative[c]):
+                    stack.pop()
+                    print('Closed    : Pop ', c,' Stack Contents: ', stack.print_stack())
+                else:
+                    isCorrect = False
+            else:
+                continue
+    
+    if(stack.get_size() != 0):
+        isCorrect = False
+        
+    print('Final Stack : Stack Contents: ', stack.print_stack())
+    print('Answer: Expression is ', 'correct' if isCorrect else 'incorrect')
